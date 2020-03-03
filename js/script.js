13d3d902c936bbce38cf356a56b0d89a2b7b079e
Quollector <60610054+Quollector@users.noreply.github.com>
2020-03-03 12:28:59 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,104 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    /* Boutons controls */
+    var btnPlay = document.querySelector('.meteor-play');
+    var btnStop = document.querySelector('.meteor-stop');
+    var btnVolumeDown = document.querySelector('.meteor-volumeDown');
+    var btnVolumeUp = document.querySelector('.meteor-volumeUp');
+
+    var iconPlay = document.querySelector('.material-play');
+    var iconPause = document.querySelector('.material-pause');
+
+    /* Vidéos */
+
+    var lecteur = document.querySelector('video');
+
+    var circleProgress = document.querySelector('#circleProgress');
+
+    var circumferenceCircle = (document.querySelector('.meteor-screen').offsetWidth)*Math.PI;
+
+    circleProgress.style.strokeDasharray = circumferenceCircle;
+    circleProgress.style.strokeDashoffset = circumferenceCircle;
+
+    /* Volume */
+
+    var element = document.querySelector('.view-volume');
+    var indiceVolume = parseInt(element.style.height);
+
+    console.log(indiceVolume);
+
+    /* Action controls */
+
+    var playOrStop = 0;    
+
+    lecteur.volume = 0.5;
+
+    btnPlay.addEventListener('click', function() {
+        if(playOrStop == 0){
+
+            lecteur.play();
+
+            iconPlay.style.display = "none";
+            iconPause.style.display = "block";
+
+            circleProgress.style.display = "block";
+            circleProgress.classList.add("progressBar");
+
+            circleProgress.style.animationDuration = lecteur.duration + "s";
+
+            playOrStop = 1;
+        }
+        else if(playOrStop == 1){           
+
+            lecteur.pause();
+
+            iconPlay.style.display = "block";
+            iconPause.style.display = "none";
+
+            console.log(circleProgress.style.strokeDasharray);
+            console.log(circleProgress.style.strokeDashoffset);
+            console.log('pause');
+
+            playOrStop = 0;
+        }
+        
+    });
+    
+    btnStop.addEventListener('click', function() {
+        lecteur.pause();
+
+        iconPlay.style.display = "block";
+        iconPause.style.display = "none";
+
+        circleProgress.style.display = "none";
+
+        playOrStop = 0;
+        lecteur.currentTime = 0;
+    })
+
+    btnVolumeDown.addEventListener('click', function(){
+            lecteur.volume = lecteur.volume - 0.1;
+
+            indiceVolume -= 10;
+
+            element.style.height = indiceVolume + '%';
+    })
+
+    btnVolumeUp.addEventListener('click', function(){
+        lecteur.volume = lecteur.volume + 0.1;
+
+        indiceVolume += 10;
+
+        element.style.height = indiceVolume + '%';
+    })
+
+    lecteur.onended = function(){
+        iconPlay.style.display = "block";
+        iconPause.style.display = "none";
+
+        circleProgress.style.display = "none";
+
+        playOrStop = 0;
+        lecteur.currentTime = 0;
+    }
+});
